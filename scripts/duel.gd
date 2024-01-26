d8c458bf5898a95eea38360ee3cc66d74164a244
SlugFiller <5435495+SlugFiller@users.noreply.github.com>
2024-01-26 14:37:11 +0200

    Add third minigame

---
+++ scripts/duel.gd
@@ -0,0 +1,48 @@
+extends Minigame
+class_name DuelMinigame
+
+const MIN_TIMING = 1
+const MAX_TIMING = 10
+const HIT_WINDOW = 0.5
+
+@onready var indicator_animation: AnimationPlayer = $IndicatorAnimation
+var hit_timing : float = 0.0
+var hit_window : float = HIT_WINDOW
+var resolution : int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	super()
+	hit_timing = randf_range(MIN_TIMING, MAX_TIMING)
+
+func minigame_start() -> void:
+	anim.play("duel")
+	indicator_animation.play("notyet")
+
+func minigame_stop() -> void:
+	indicator_animation.play("RESET")
+
+func minigame_process(delta: float) -> void:
+	if resolution == 1:
+		do_lose()
+		return
+	if resolution == 2:
+		do_win()
+		return
+	if hit_timing > 0:
+		if hit_timing > delta:
+			hit_timing -= delta
+		else:
+			hit_timing = 0
+			anim.stop(true)
+			indicator_animation.play("doit")
+	elif hit_window > delta:
+		hit_window -= delta
+	else:
+		do_lose()
+
+func action() -> void:
+	if hit_timing > 0:
+		resolution = 1
+	else:
+		resolution = 2
--- scripts/hub.gd
+++ scripts/hub.gd
@@ -1,17 +1,31 @@
 extends Node2D
 
+const MINIGAMES : Array[PackedScene] = [
+	preload("res://minigames/umbrella.tscn"),
+	preload("res://minigames/drive.tscn"),
+	preload("res://minigames/duel.tscn"),
+]
+
 var difficulty : int = 0
+var unused_minigames : Array[int] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var minigame : Minigame = preload("res://minigames/umbrella.tscn").instantiate()
-	minigame.next.connect(self._next)
-	add_child(minigame)
-
+	for i in MINIGAMES.size():
+		unused_minigames.push_back(i)
+	_run_minigame()
 
 func _next() -> void:
 	difficulty += 1
-	var minigame : Minigame = preload("res://minigames/drive.tscn").instantiate()
+	_run_minigame()
+	
+func _run_minigame() -> void:
+	if unused_minigames.size() == 0:
+		return
+	var which : int = randi_range(0, unused_minigames.size()-1)
+	var game : int = unused_minigames[which]
+	unused_minigames.remove_at(which)
+	var minigame : Minigame = MINIGAMES[game].instantiate()
 	minigame.difficulty = difficulty
 	minigame.next.connect(self._next)
 	add_child(minigame)
